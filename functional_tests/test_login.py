62c51449e0bb92c01299e264d03bb06db658fdec
Peter Simpson <peter_joseph_simpson@hotmail.com>
2021-02-01 22:01:20 +0000

    correct testing email address

--- functional_tests/test_login.py
+++ functional_tests/test_login.py
@@ -45,7 +45,7 @@ class LoginTest(FunctionalTest):
         # and notices a "Log in" section in the navbar for the first time
         # It's telling her to enter her email address, so she does
         if self.staging_server:
-            test_email = 'edith.testuser@yahoo.com'
+            test_email = 'peterjosephsimpson1987@gmail.com'
         else:
             test_email = 'edith@example.com'
 
