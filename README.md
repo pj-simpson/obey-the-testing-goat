92c9a792772d9e379165a88d3f1ad721d9b36cd6
Peter Simpson <peter_joseph_simpson@hotmail.com>
2021-02-12 20:17:37 +0000

    added readme, finished off the main chapters from the book.

---
+++ README.md
@@ -0,0 +1,4 @@
+The is the code from having worked through Harry Percival's excellent book on TDD.
+https://www.oreilly.com/library/view/test-driven-development-with/9781491958698/ 
+Its a simple Django app, but the book represents an informed and fun discussion of 'Outside-In' TDD, isolation, mocking,
+Integrated vs Integration, etc ,etc, etc.... I would recommend this book to everyone interested in Python web development.
--- superlists/settings.py
+++ superlists/settings.py
@@ -147,12 +147,11 @@ LOGGING = {
     'root': {'level': 'INFO'},
 }
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 
-# EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_HOST_USER = os.environ.get('GMAIL_ACCOUNT')
-# EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
-# EMAIL_PORT = 587
-# EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = os.environ.get('GMAIL_ACCOUNT')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
 
